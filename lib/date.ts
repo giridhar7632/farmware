6c9fc924873033c974a1a35bcd2e23d3ca4a6492
Sabih S <67878058+kleenkanteen@users.noreply.github.com>
2024-03-18 11:59:08 -0400

    remove auto 7 month subtraction now that datepicker is added (#10)

--- lib/date.ts
+++ lib/date.ts
@@ -1,7 +1,5 @@
 export function addDays(date: Date, days: number) {
   const result = new Date(date)
-  // subtract 7 months form the date
-  result.setMonth(result.getMonth() - 7)
   result.setDate(result.getDate() + days)
   return result
 }
